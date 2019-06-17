8539a2af0a188b42a1885d3992a811926430bf15
vickyinguo <39468960+vickyinguo@users.noreply.github.com>
2019-06-16 19:48:21 -0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,8 +1,8 @@
 # This application shows the relationship between car weight and mpg using mtcars dataset in R.
-  
-  # It has 2 histograms which you can change number of bins to see how car weight and mpg correlates, 
-  # and a regression model which you can change number of points in the model. You can also see the R squre value of 
-  # the regression model - a measure of fitness of the model - when you change the number of points in the model.
+
+# It has 2 histograms which you can change number of bins to see how car weight and mpg correlates, 
+# and a regression model which you can change number of points in the model. You can also see the R squre value of 
+# the regression model - a measure of fitness of the model - when you change the number of points in the model.
 
 library(shiny)
 
@@ -10,11 +10,12 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Predict mtcars MPG"),
+  titlePanel("mtcars Weight vs MPG"),
   
   # Sidebar with a slider input for number of bins and number of points in the regression
   sidebarLayout(
     sidebarPanel(
+       
        h3("Select number of bins"),
        sliderInput("bins",
                    "Number of bins:",
@@ -23,7 +24,8 @@ shinyUI(fluidPage(
                    value = 30),
        checkboxInput("show_xlab","Show/Hide X Axis Label",value=TRUE),
        checkboxInput("show_ylab","Show/Hide Y Axis Label",value=TRUE),
-       checkboxInput("show_title","Show/Hide Title",value=TRUE),
+       checkboxInput("show_title","Show/Hide Title"),
+       
        h3("Select number of points in the regression"),
        sliderInput("points",
                    "Number of points:",
@@ -38,11 +40,15 @@ shinyUI(fluidPage(
     
     # Show a plot of the generated distribution and the regression model
     mainPanel(
-       h3("Weight Histogram"),
+       h3("This application shows the relationship between car weight and mpg using mtcars dataset in R."),
+       h4("This section has 2 histograms which you can change number of bins to see how car weight and mpg correlates 
+          when changing bins"),
+       h4("Weight Histogram"),
        plotOutput("distPlot1"),
-       h3("MPG Histogram"),
+       h4("MPG Histogram"),
        plotOutput("distPlot2"),
-       h3("Regression"),
+       h4("This section has a regression model which you can change number of points in the model to see how the model fits"),
+       h4("Regression"),
        plotOutput("distPlot3")
        
     )
